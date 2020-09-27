2ea34a01ddd50dea4a5a56d9e902335f0a526648
Anton Popov <a.popov@kit.team>
2020-09-28 00:30:56 +0500

    Класс по дефолту

--- src/Exceptions/RegExceptions.php
+++ src/Exceptions/RegExceptions.php
@@ -5,19 +5,4 @@ namespace RegApi\Exceptions;
 use Exception;
 
 class RegExceptions extends Exception
-{
-    /**
-     * @param Exception $e
-     *
-     * @return $this
-     */
-    public static function render(Exception $e)
-    {
-        switch ($e->message) {
-            default:
-                $return = redirect()->back()->withErrors([$e->getMessage()]);
-        }
-
-        return $return;
-    }
-}
+{}
